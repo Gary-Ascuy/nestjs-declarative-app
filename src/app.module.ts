6577c9421ed0cad105dbc1daceccbf6c2e02c0d7
Gary Ascuy <gary.ascuy@gmail.com>
2025-05-03 08:28:21 -0500

    Refactor UsersController to utilize PetService and update AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
+import { ApiModule } from '../generated-sources/clients/petstore/api.module';
 
 @Module({
-  imports: [],
+  imports: [ApiModule],
   controllers: [AppController, UsersController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
+import { PetService } from 'generated-sources/clients/petstore';
+import { firstValueFrom } from 'rxjs';
 
 @Controller('users')
 export class UsersController {
+    constructor(private readonly petService: PetService) { }
+
     @Get()
-    getUsers() {
-        return "s"
+    async getUsers() {
+        const { data: listOfPets } = await firstValueFrom(this.petService.findPetsByStatus(['available']))
+        return listOfPets
     }
 }
